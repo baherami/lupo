d8fdcea2730483a3b07b061767eb0773c19b69dc
Bahrami <baherami@gmail.com>
2020-12-02 20:02:42 +0200

    add dataset mark duplicate methods

--- dataset_tools.py
+++ dataset_tools.py
@@ -3,6 +3,9 @@ import os
 import argparse
 from PIL import Image
 import logging
+import shutil
+import imagehash
+
 
 SEARCH_URL = "https://api.bing.microsoft.com/v7.0/images/search"
 NUMBER_OF_IMAGE_PER_PAGE = 50
@@ -14,6 +17,7 @@ class MessageLevel:
     WARNING = "WARNING"
 
 
+reference_image_hashes = {}
 logger = logging.getLogger(__name__)
 
 
@@ -48,7 +52,7 @@ def retrieve_image(img_url, img_number):
             end_index = None
 
         extension = img_url[start_index:end_index]
-        image_path = os.path.sep.join([output_location, f"{str(img_number).zfill(8)}{extension}"])
+        image_path = os.path.sep.join([output_directory, f"{str(img_number).zfill(8)}{extension}"])
         with open(image_path, "wb") as f:
             f.write(request_content.content)
     except Exception:
@@ -59,9 +63,10 @@ def retrieve_image(img_url, img_number):
 
 def pars_arguments():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("-q", "--query", help="search query", required=True)
-    arg_parser.add_argument("-o", "--output", help="output path", required=True)
-    arg_parser.add_argument("-s", "--subscription", help="subscription key for azure", required=True)
+    arg_parser.add_argument("-q", "--query", help="search query", default="puppy")
+    arg_parser.add_argument("-o", "--output", help="output path", default="./output/")
+    arg_parser.add_argument("-i", "--input", help="input path", default="./input")
+    arg_parser.add_argument("-s", "--subscription", help="subscription key for azure", default="some_key")
     arg_parser.add_argument("-a", "--action", help="action to do", required=True)
     arguments = vars(arg_parser.parse_args())
     return arguments
@@ -103,14 +108,54 @@ def get_images():
             img_counter += 1
 
 
+def load_image(file_path):
+    try:
+        img = Image.open(file_path)
+    except Exception:
+        print_status(f"{file_path} is not a valid image, remove it!", MessageLevel.WARNING)
+        return None
+    return img
+
+
+def make_reference_hashes():
+    print_status("referencing")
+    reference_files = os.listdir(output_directory)
+    for ref_file in reference_files:
+        img = load_image(output_directory+ref_file)
+        if not img:
+            continue
+        img_hash = imagehash.average_hash(img)
+        reference_image_hashes.update({img_hash:ref_file})
+
+
+def mark_duplicates():
+    print_status("marking")
+    input_files = os.listdir(input_directory)
+    for in_file in input_files:
+        img = load_image(input_directory + in_file)
+        if not img:
+            continue
+        img_hash = imagehash.average_hash(img)
+        ref_data = reference_image_hashes.get(img_hash, None)
+        if not ref_data:
+            shutil.move(input_directory + in_file, output_directory + str(img_hash) + in_file )
+            reference_image_hashes.update({img_hash:in_file})
+        else:
+            print_status(f"Image {in_file} is not unique. duplicate of {ref_data}")
+
+
 if __name__ == "__main__":
     args = pars_arguments()
     search_term = args["query"]
-    output_location = args["output"]
+    output_directory = args["output"]
+    input_directory = args["input"]
     subscription_key = args["subscription"]
     action = args["action"]
 
     if action == "get_images":
         get_images()
+    if action == "mark_duplicates":
+        make_reference_hashes()
+        mark_duplicates()
     else:
         raise NotImplementedError("Action not found")
